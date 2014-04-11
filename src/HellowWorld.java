80f9cc3b9f5a24cadbfc45216eb2be88302899c6
g12908hm <g12908hm@s30410.tsuda.ac.jp>
2014-04-11 11:03:33 +0900

    新規にHelloWorldを作成しました

---
+++ src/HellowWorld.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author g12908hm
+ */
+public class HellowWorld {
+    public static void main(String[]args){
+        System.out.println("HelloWorld");
+    }
+}
